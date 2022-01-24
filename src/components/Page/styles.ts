b41940f3bff6b9af7974030cd924464c464abeef
Nick Barone <xnickbarone@gmail.com>
2022-01-23 21:15:19 -0500

    Fix page width

--- src/components/Page/styles.ts
+++ src/components/Page/styles.ts
@@ -3,5 +3,5 @@ import styled from 'styled-components';
 export const Container = styled.div`
   background-color: ${(p) => p.theme.page.backgroundColor};
   margin-left: ${(p) => p.theme.navigation.width};
-  width: calc(100vw - 250px);
+  width: calc(100vw - ${(p) => p.theme.navigation.width});
 `;
